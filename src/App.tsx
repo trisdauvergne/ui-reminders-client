df8a281e2dafa68ce8d403ca9cf6dc4f44574501
trisdauvergne <tristan.dauvergne@gmail.com>
2022-04-01 15:33:43 +0200

    Testing axios in default welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState
+} from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [testData, setTestData] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/test`)
+      .then((res) => {
+        console.log(res.data);
+        setTestData(res.data.message);
+      })
+  }, []);
+
+  if (!testData) {
+    return (
+      <h1>Loading...</h1>
+    )
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>{!testData ? `Womp womp` : testData}</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
