8984f8e8f802f58cea647334d25fd38bd3765b33
trisdauvergne <tristan.dauvergne@gmail.com>
2022-04-06 15:27:03 +0200

    Passing JSX data to modal from list for a reminder

--- src/components/listItem/ListItem.tsx
+++ src/components/listItem/ListItem.tsx
@@ -1,13 +1,20 @@
-import React, { useState} from 'react';
+import React, {
+  useState
+} from 'react';
 import axios from 'axios';
 import { host } from '../../utils/config';
 import { IList } from '../../interfaces/List';
-import { useDispatch } from 'react-redux';
+import { IReminder } from '../../interfaces/Reminder';
+import {
+  useDispatch,
+} from 'react-redux';
 import { addListsToState } from '../../redux/listsSlice';
-import ReminderModal from '../modals/reminderModal/ReminderModal';
+import Modal from '../modal/Modal';
+import { v4 as uuidv4 } from 'uuid';
 
 const ListItem = (list: IList) => {
-  const [ reminderModalVisible, setReminderModalVisible ] = useState(false);
+    const [ reminderModalVisible, setReminderModalVisible ] = useState(false);
+    const [ reminder, setReminder ] = useState<string>('');
     const dispatch = useDispatch();
 
     const refreshPage = async () => {
@@ -17,11 +24,6 @@ const ListItem = (list: IList) => {
         dispatch(addListsToState(listData));
       })
     };
-
-    const addReminderToList = async () => {
-      console.log('in add reminder to list function');
-      setReminderModalVisible(!reminderModalVisible);
-    }
     
     const deleteList = async () => {
       console.log('in deleteList', list, `${host}/${list.id}`);
@@ -32,14 +34,50 @@ const ListItem = (list: IList) => {
       refreshPage();
     };
 
+    const sendReminderToBackEnd = (reminderToAdd: IReminder) => {
+      console.log('sending to backend', reminderToAdd);
+    }
+
+    const createReminderObject = (e: any) => {
+      e.preventDefault();
+      const id = uuidv4().replace(/-/g, "");
+      const reminderToAdd: IReminder = {
+        description: reminder,
+        id,
+        notes: []
+      };
+      sendReminderToBackEnd(reminderToAdd);
+      setReminder('');
+      setReminderModalVisible(false);
+    }
+
+    const modalJsx = 
+      <>
+        <h1>Add a reminder to {list.name}</h1>
+        <p>{list.id}</p>
+        <form onSubmit={(e) => createReminderObject(e)}>
+          <label>Description</label>
+          <p>Describe your reminder</p>
+          <input
+            type="text"
+            name="reminder"
+            onChange={e => setReminder(e.target.value)}
+            value={reminder}
+          />
+          <button type="submit">Add reminder</button>
+        </form>
+        <button onClick={() => setReminderModalVisible(false)}>Hide modal</button>
+      </>
+    ;
+
     return (
       <div>
-          {reminderModalVisible && <ReminderModal {...list}/>}
+        {reminderModalVisible && <Modal children={modalJsx} />}
           <h3>{list.name}</h3>
           <p>{list.description}</p>
           <p>ID: {list.id}</p>
           <button>Hide completed</button>
-          <button onClick={addReminderToList}>Add reminder to {list.name}</button>
+          <button onClick={() => setReminderModalVisible(true)}>Add reminder to {list.name}</button>
           <button onClick={deleteList}>Delete {list.name}</button>
           <p>Reminders: {list.reminders && list.reminders.length > 0 ? `There are ${list.reminders.length} reminders in your list` : `No reminders`}</p>
           <button>View more</button>
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectModal, changeModalVisibility } from '../../redux/modalSlice';
+import './modal.scss';
 
 const Modal: React.FC = ({children}) => {
-    const modalVisible = useSelector(selectModal);
-    const dispatch = useDispatch();
-
-    const hideModal = () => {
-        dispatch(changeModalVisibility(false))
-    }
-
-    if (modalVisible) {
-        return (
-            <section>
-                <h1>Modal</h1>
-                <button onClick={hideModal}>Hide modal</button>
-                {children}
-            </section>
-        )
-    } else {
-        return (
-            <section></section>
-        )
-    }
+    return (
+        <section className="modal">
+            {children}
+        </section>
+    )
 }
 
 export default Modal;
