d03f3790f69a876ee06d48825d0aead863e5ea43
trisdauvergne <tristan.dauvergne@gmail.com>
2022-04-10 19:08:01 +0200

    Reintroduced modal for viewing list information when screen size is below 768px

--- src/components/viewLists/ViewLists.tsx
+++ src/components/viewLists/ViewLists.tsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import {
+  useEffect,
+  useState
+} from 'react';
 import {
   Link
 } from 'react-router-dom';
@@ -10,14 +13,33 @@ import {
 import {
   selectLists,
   addListsToState,
+  saveListToState
 } from '../../redux/listsSlice';
+import {
+  selectViewMoreModal,
+  changeViewMoreModalVisibility
+} from '../../redux/modalSlice';
 import { host } from '../../utils/config';
+import ModalViewMore from '../modalViewMore/ModalViewMore';
 import { IList } from '../../interfaces/List';
 import './viewlists.scss';
 
 const ViewLists = () => {
+  const [ width, setWidth ] = useState(window.innerWidth);
   const lists = useSelector(selectLists);
+  const viewMoreModal = useSelector(selectViewMoreModal);
   const dispatch = useDispatch();
+
+  const breakPoint = 768;
+
+  const showViewMoreModal = (list: IList) => {
+    dispatch(saveListToState(list));
+    dispatch(changeViewMoreModalVisibility(true));
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setWidth(window.innerWidth))
+  }, []);
 
   useEffect(() => {
     axios.get(`${host}/lists`)
@@ -31,10 +53,14 @@ const ViewLists = () => {
     return (
       <section className="view-lists">
           <h1>Your lists</h1>
-          {lists.map((list: IList, i: number) => 
+          {width > breakPoint ? lists.map((list: IList, i: number) => 
             <Link to={`/viewlist/${list.id}`} key={i}>{list.name}</Link>
             )
-          }
+          : 
+          lists.map((list: IList, i: number) => 
+          <button onClick={() => showViewMoreModal(list)}>{list.name}</button>
+          )}
+          {viewMoreModal && <ModalViewMore />}
       </section>
     )
   } else {
